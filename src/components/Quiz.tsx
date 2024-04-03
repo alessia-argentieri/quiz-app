5d3e482d7c7f4946cbdc69991769745f4ef6c46f
alex arg <alex.arg.ax@gmail.com>
2024-04-03 15:40:46 +0100

    Add index, score, selectedAnswer and finished state; add highlightAnswer function

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -6,6 +6,25 @@ type Props = {
 };
 
 const Quiz: FC<Props> = ({ questions }) => {
+  const [index, setIndex] = useState(0); //index of Question
+  const [score, setScore] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const [finished, setFinished] = useState(false);
+
+  const question = questions[index]; //Question
+
+  const highlightAnswer = (answerIndex: number): string | undefined => {
+    if (selectedAnswer === null) {
+      return undefined;
+    } else if (answerIndex === question.answer) {
+      return "correct";
+    } else if (
+      answerIndex === selectedAnswer &&
+      answerIndex !== question.answer
+    ) {
+      return "wrong";
+    }
+  };
   return <div></div>;
 };
 
